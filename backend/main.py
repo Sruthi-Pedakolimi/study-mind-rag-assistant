da443503dcb50c9e35c39b09cc9d19f357cfad9e
Sruthi <sruthipedakolimi@gmail.com>
2026-07-04 11:57:50 -0500

    Recursive chunking with overlap (using LangChain)

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,16 @@ from openai import OpenAI
 from fastapi import FastAPI, UploadFile
 from dotenv import load_dotenv
 from pypdf import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 load_dotenv(override=True)
 app = FastAPI()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY") , base_url=os.getenv("OPENAI_BASE_URL"))
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=2000,
+    chunk_overlap=200,
+)
 
 @app.post("/upload")
 async def get_file(file: UploadFile):
@@ -20,11 +25,11 @@ async def get_file(file: UploadFile):
     reader = PdfReader(file.file)
     for page in reader.pages:
         extracted_page = page.extract_text()
+        # avoid none extraction
         if  extracted_page:
             text += extracted_page
-
     
-   
-    
-    return {"filename": file.filename, "text_preview": text[:200]}
+    chunks = text_splitter.split_text(text)
+            
+    return {"filename": file.filename, "chunks": chunks}
 
